afb5a08f15d8046407e04e815d9de030aa5db405
Rohan Kadu <rohank286@gmail.com>
2019-02-25 22:24:41 +0000

    Initial Project Structure Commit

---
+++ address-book/src/main/java/com/codingtest/addressbook/AddressBookApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.codingtest.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ROHAN-PC
+ *
+ */
+@SpringBootApplication
+public class AddressBookApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+
+}
